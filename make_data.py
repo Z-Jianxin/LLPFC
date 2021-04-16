cb2511815842e4713684dffc522c33fe6ba4f78a
Z-Jianxin <jianxinz@umich.edu>
2021-04-16 18:03:02 -0400

    argument parser

--- make_data.py
+++ make_data.py
@@ -14,10 +14,11 @@ class InvalidArguments(Exception):
 def get_args():
     parser = argparse.ArgumentParser(description="Partition data into bags for LLP")
     # required:
-    parser.add_argument("--dataset", nargs='?', choices=["cifar10"], required=True,
+    parser.add_argument("-d", "--dataset", nargs='?', choices=["cifar10"], required=True,
                         help="name of the dataset, the program uses torchvision.datasets")  # add more data sets later
-    parser.add_argument("--num_classes", nargs='?', type=int, required=True, metavar="10", help="number of classes")
-    parser.add_argument("--data_save_name", nargs='?', required=True, metavar="cifar10_1024_0",
+    parser.add_argument("-c", "--num_classes", nargs='?', type=int, required=True, metavar="10",
+                        help="number of classes")
+    parser.add_argument("-s", "--data_save_name", nargs='?', required=True, metavar="cifar10_1024_0",
                         help="name of the file to save")
 
     # optional:
@@ -26,15 +27,15 @@ def get_args():
                         help="path to the folder of original data, if not exists, the dataset will be downloaded")
     parser.add_argument("--data_folder_llp", nargs='?', default="../data/llp_data/", metavar="../data/llp_data/",
                         help="path to save the training data for llp")
-    parser.add_argument("--method", nargs='?', default="dirichlet", choices=["dirichlet", "uniform"],
-                        help="method to generate bags")  # dirichlet, uniform # Todo: implement "counts"
-    parser.add_argument("--alpha", nargs="?", default="equal", choices=["equal"],
-                        help="parameter of dirichlet distribution; required if use dirichlet to generate bags")  # add more to this
-    parser.add_argument("--bag_size", nargs='?', type=int, metavar="1024",
+    parser.add_argument("-m", "--method", nargs='?', default="dirichlet", choices=["dirichlet", "uniform"],
+                        help="method to generate bags")  # dirichlet, uniform
+    parser.add_argument("-a", "--alpha", nargs="?", default="equal", choices=["equal"],
+                        help="parameter of dirichlet distribution; required if use dirichlet to generate bags")  # Todo: add more to this
+    parser.add_argument("-b", "--bag_size", nargs='?', type=int, metavar="1024",
                         help="size of bag; note not all bag sizes will equal to this number if use dirichlet")
-    parser.add_argument("--num_bags", nargs='?', type=int, metavar="100",
+    parser.add_argument("-n", "--num_bags", nargs='?', type=int, metavar="100",
                         help="number of bags to generate; it too large, the dataset may have insufficient data points")
-    parser.add_argument("--seed", nargs='?', type=int, metavar="0",
+    parser.add_argument("-r", "--seed", nargs='?', type=int, metavar="0",
                         help="seed of RNG for both random and numpy.random")  # both random and numpy.random will use this seed
     return parser.parse_args()
 
